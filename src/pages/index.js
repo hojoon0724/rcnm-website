5e4ae2acffcde9c688a61128fdba1ed66ab9a914
hojoon0724 <hojoon0724@gmail.com>
2024-09-25 21:11:08 -0700

    loop accessing 2 db done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import BackgroundVideo from 'next-video/background-video';
-import NavBar from '@/components/NavBar/NavBar';
 import { musicians } from '@/database/musicians/musicians';
 import { s03 } from '@/database/season/season3';
 
@@ -10,9 +9,27 @@ export default function Home() {
       <main>
         <div className="topContainer">
           <div className="landingTextContainer">
-            {s03.map(e => (
-              <div className="eventContainer" key={e.title}>
-                <div className="eventTitle">{e.title}</div>
+            {s03.map(event => (
+              <div className="eventContainer" key={event.title}>
+                <div className="eventTitle">{event.title}</div>
+                <div className="musiciansContainer">
+                  {event.musicians.map(musicianKey => {
+                    const musician = musicians[musicianKey];
+                    return (
+                      <div className="musician" key={musicianKey}>
+                        <div className="musicianName">
+                          {musician.firstName} {musician.lastName}
+                        </div>
+                        <div className="musicianInstrument">{musician.instrument}</div>
+                        <Image
+                          className="musicianPhoto"
+                          src={musician.photo}
+                          alt={`${musician.firstName} ${musician.lastName}`}
+                        />
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
             ))}
           </div>
